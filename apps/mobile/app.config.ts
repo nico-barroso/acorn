04bad5fc8f96b8524ddcaced02f6bc6d66308488
Nico <josenicolasbarrosogarcia@gmail.com>
2026-05-02 14:41:00 +0100

    fix(package):expo-linear-gradient deprecated

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -35,6 +35,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   plugins: [
     'expo-router',
+    'expo-dev-client',
     'expo-web-browser',
     [
       'expo-image-picker',
